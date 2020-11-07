965b68a15ed344cb3ad9d78a48f43e499a767f80
CovarianceMomentum <covariancemomentum@gmail.com>
2020-11-07 13:02:07 +0300

    Command init restructured

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ import telebot
 
 import dice_parser
 
-
 bot = telebot.TeleBot(os.getenv('BOT_TOKEN'))
 
 
@@ -34,49 +33,60 @@ class Command:
 commands: List[Command] = []
 
 
-def init_commands():
-    def help_command_f(message):
-        if message.text[5:].isspace() or len(message.text) == 5:
-            bot.send_message(message.chat.id,
-                             '\n\n'.join(list(map(lambda x: x.help_prompt, commands))))
-        else:
-            cmd = message.text[5:].split(' ')[1]
-            if cmd not in list(map(lambda x: x.name, commands)):
-                bot.send_message(message.chat.id,
-                                 'Command not found!\n\n'
-                                 'Try using /help to get list of all commands.')
-            else:
-                bot.send_message(message.chat.id,
-                                 [x.help_full for x in commands if x.name == cmd][0])
-
-    help_command_prompt = 'prints this message or help for command if command name is provided'
-    help_command_full = 'prints help for all command or help' \
-                        ' for one command if command name is provided. ' \
-                        'Usage: /help | /help <command>.' \
-                        ' If command is omitted, prints help prompts for all commands.'
-    commands.append(Command('help', help_command_f, help_command_prompt, help_command_full))
-
-    def start_command_f(message):
+def __help_command_f(message):
+    if message.text[5:].isspace() or len(message.text) == 5:
         bot.send_message(message.chat.id,
-                         'Hey!\nI am DND Organizer, '
-                         'bot that helps you with your character management. '
-                         'Write \"/help\" to learn what I can do!')
+                         '\n\n'.join(list(map(lambda x: x.help_prompt, commands))))
+    else:
+        cmd = message.text[5:].split(' ')[1]
+        if cmd not in list(map(lambda x: x.name, commands)):
+            bot.send_message(message.chat.id,
+                             'Command not found!\n\n'
+                             'Try using /help to get list of all commands.')
+        else:
+            bot.send_message(message.chat.id,
+                             [x.help_full for x in commands if x.name == cmd][0])
 
-    start_command_prompt = 'prints welcoming message'
-    start_command_full = 'prints welcoming message. Usage: /start.'
-    commands.append(Command('start', start_command_f, start_command_prompt, start_command_full))
 
-    def dice_command_f(message):
-        bot.send_message(message.chat.id, dice_parser.parse_expr(message.text[5:]))
+__help_command_prompt = 'prints this message or help for command if command name is provided'
+__help_command_full = 'prints help for all command or help ' \
+                      'for one command if command name is provided. ' \
+                      'Usage: /help | /help <command>. ' \
+                      'If command is omitted, prints help prompts for all commands.'
 
-    dice_command_prompt = 'rolls specified dice. Can be used via \"!\"'
-    dice_command_full = 'rolls specified dice. Can be used via \"!\". ' \
-                        'Dice rolls syntax: XdY to roll X Y-sided dice. ' \
-                        'You can use \"+\" and \"-\" arithmetic operations ' \
-                        'in between of rolls. Constants also can be used. ' \
-                        'Do not worry about whitespaces - they are ' \
-                        'ignored. \nExamples:\n/roll 1d20 - 1d4 + 5\n! 1d20 - 1d4 + 5'
-    commands.append(Command('dice', dice_command_f, dice_command_prompt, dice_command_full))
+
+def __start_command_f(message):
+    bot.send_message(message.chat.id,
+                     'Hey!\nI am DND Organizer, '
+                     'bot that helps you with your character management. '
+                     'Write \"/help\" to learn what I can do!')
+
+
+__start_command_prompt = 'prints welcoming message'
+__start_command_full = 'prints welcoming message. Usage: /start.'
+
+
+def __dice_command_f(message):
+    bot.send_message(message.chat.id, dice_parser.parse_expr(message.text[5:]))
+
+
+__dice_command_prompt = 'rolls specified dice. Can be used via \"!\"'
+__dice_command_full = 'rolls specified dice. Can be used via \"!\". ' \
+                      'Dice rolls syntax: XdY to roll X Y-sided dice. ' \
+                      'You can use \"+\" and \"-\" arithmetic operations ' \
+                      'in between of rolls. Constants also can be used. ' \
+                      'Do not worry about whitespaces - they are ' \
+                      'ignored. \nExamples:\n/roll 1d20 - 1d4 + 5\n! 1d20 - 1d4 + 5'
+
+
+def init_commands():
+    commands.extend(
+        [
+            Command('help', __help_command_f, __help_command_prompt, __help_command_full),
+            Command('start', __start_command_f, __start_command_prompt, __start_command_full),
+            Command('dice', __dice_command_f, __dice_command_prompt, __dice_command_full)
+        ]
+    )
 
     for command in commands:
         command.init()
